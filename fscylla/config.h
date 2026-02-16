614453598f41399b0af4ea0aae7f258d425a5355
Florian Laporte <fnle@fnle.be>
2026-02-16 16:11:32 +0100

    lala

--- fscylla/config.h
+++ fscylla/config.h
@@ -2,9 +2,12 @@
 #define HOLD_ON_OTHER_KEY_PRESS
 
 // Split config
-#define SERIAL_USART_FULL_DUPLEX   // Enable full duplex
-#define SERIAL_USART_TX_PIN D2     // The original half-duplex pin going to aux connector on the splinktegrated
-#define SERIAL_USART_RX_PIN D3     // Originally used for RGB on splinktegrated, but I repurposed it for full duplex (jumper to aux)
+#define SERIAL_USART_FULL_DUPLEX // Enable full duplex
+#define SERIAL_USART_TX_PIN GP0  // RGB pin repurposed for full duplex serial
+#define SERIAL_USART_RX_PIN GP1  // Serial pin on audio jack repurposed for full duplex
+#define SPLIT_USB_DETECT       // Master is whichever half has USB connected
+#define SPLIT_HAND_PIN GP20    // LOW/GND = left half, HIGH/floating = right half
+#define	SPLIT_HAND_PIN_LOW_IS_LEFT
 
 // Unicode input modes: Linux and MacOS
 #define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX, UNICODE_MODE_MACOS
--- fscylla/keymaps/default/keymap.c
+++ fscylla/keymaps/default/keymap.c
@@ -1,6 +1,5 @@
 #include QMK_KEYBOARD_H
 #include <stdint.h>
-#include "keymap.h"
 #include "action.h"
 #include "keycodes.h"
 #include "print.h"
